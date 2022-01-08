93ade893437db6f58f5fd760abe35872d240b373
Suyeol Yun <syyun@mit.edu>
2022-01-09 01:35:10 +0900

    add bs4

--- crawl.py
+++ crawl.py
@@ -5,6 +5,9 @@ url = "https://en.wikipedia.org/wiki/List_of_North_Korean_missile_tests"
 response = requests.get(url)
 html = response.text
 bs = BeautifulSoup(html, "html.parser")
+tables = bs.find_all("table")
+wiki_table = tables[1]
+wiki_table_rows = wiki_table.find_all("tr")
 
 if __name__ == "__main__":
     pass
